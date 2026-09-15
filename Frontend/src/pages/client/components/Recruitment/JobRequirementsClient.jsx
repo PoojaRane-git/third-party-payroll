ed4aa6e1b29babd1b86b182d74779574d36b2be7
Pooja Rane <poojarane514@example.com>
2026-09-15 16:58:57 +0530

    fixed client

--- Frontend/src/pages/client/components/Recruitment/JobRequirementsClient.jsx
+++ Frontend/src/pages/client/components/Recruitment/JobRequirementsClient.jsx
@@ -15,7 +15,7 @@ import {
 
 import Sidebar from "../Layout/Sidebar";
 import api from "../../../services/api";
-import { useAuth } from "../../../auth/AuthProvider";
+import { useAuth } from "../../../../auth/AuthProvider";
 
 // ============================================================
 // COMPONENT
