f4767a5f4666e9567159212e974ee7d7d32f1a61
Pooja Rane <poojarane514@example.com>
2026-09-16 17:53:59 +0530

    fixed client

--- Frontend/src/pages/admin/Deployment/EmployeeDetails.jsx
+++ Frontend/src/pages/admin/Deployment/EmployeeDetails.jsx
@@ -21,7 +21,7 @@ import {
 } from "lucide-react";
 
 import Sidebar from "../Layout/Sidebar";
-import api from "../../../services/api";
+import api from "../../services/api";
 
 export default function EmployeeDetails() {
   const { id } = useParams();
