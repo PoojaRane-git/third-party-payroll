1beb3b31dd9da64cfd9c68311f7094aa704de67f
Pooja Rane <https://github.com/PoojaRane-git>
2026-09-24 15:36:24 +0530

    chnages

--- backend/routes/employee/employeeAttendance.js
+++ backend/routes/employee/employeeAttendance.js
@@ -20,8 +20,7 @@ const {
     getMonthRange,
     DAILY_COLUMNS,
     SUMMARY_COLUMNS,
-} = require("../../services/attendanceSummary");
-
+} = require("../services/attendanceSummary")
 router.use(authenticate, authorize("employee"));
 
 // ============================================================
